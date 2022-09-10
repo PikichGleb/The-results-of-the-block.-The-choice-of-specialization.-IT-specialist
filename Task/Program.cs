9f0dd4f7e2db35efc2e1817e784f7937bb1ef868
PikichGleb <PikichGleb@yandex.ru>
2022-09-10 17:10:13 +0300

    NewArray + .drawio

--- Task/Program.cs
+++ Task/Program.cs
@@ -9,8 +9,22 @@ string[] array1 = new string[] { "Hello", "2", "world", ":-)" };
 string[] array2 = new string[array1.Length];
 
 
-
+NewArray(array1,array2);
 PrintArray(array2);
+
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
 
 void PrintArray(string[] array)
 {
